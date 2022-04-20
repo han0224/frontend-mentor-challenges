80ada551420e2c46c41cee7c9ad12cad5610f0f7
han0224 <dbn0224@gmail.com>
2022-04-20 22:47:41 +0900

    다시

--- index.js
+++ index.js
@@ -8,17 +8,33 @@ const projects = [
     {
         name: 'interactive-rating-page-app',
     },
+    // {
+    //     name: 'interactive-rating-page-app',
+    // },
+    // {
+    //     name: 'interactive-rating-page-app',
+    // },
+    // {
+    //     name: 'interactive-rating-page-app',
+    // },
+    // {
+    //     name: 'interactive-rating-page-app',
+    // },
+    // {
+    //     name: 'interactive-rating-page-app',
+    // },
 ]
 
 const list = document.querySelector('.list');
 
 projects.forEach(v=>{
     const listItem = document.createElement('div');
-    listItem.className = 'listItem'
+    listItem.className = 'list-item'
     listItem.innerHTML=`<a href=/${v.name}/index.html>
-    <div class='instar'></div>
-    <img class="screenshot" src='/${v.name}/design/screenshot.png' alt="${v.name}" width='200' height='200'>
-    <p>${v.name}</p>
+        <div class='instar'>
+        </div>
+        <img class="preview" src='/${v.name}/design/desktop-preview.jpg' alt="${v.name}" width='200' height='200'>
+        <p>${v.name}</p>
     </a>
     `
     list.appendChild(listItem);
