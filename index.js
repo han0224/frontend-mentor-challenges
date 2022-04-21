8e415dfa4b56cef8bc6fa9d1a57ce8827e0dff7b
han0224 <70616579+han0224@users.noreply.github.com>
2022-04-22 00:30:44 +0900

    수정 중

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const projects = [
     {
-        name: 'calculator-app',
+        name: 'interactive-rating-page-app',
+        date: '2022-03-22',
+        blog: 'https://han0224.github.io/frontend%20mentor/Interactive-rating-page/',
     },
     {
         name: 'advice-generator-app',
+        date: '2022-03-26',
+        blog: 'https://han0224.github.io/frontend%20mentor/advice-generator-page/',
     },
+    
     {
-        name: 'interactive-rating-page-app',
+        name: 'calculator-app',
+        date: '2022-04-11',
+        blog: 'https://han0224.github.io/frontend%20mentor/calculator-app/',
     },
+    
     // {
     //     name: 'interactive-rating-page-app',
     // },
@@ -25,41 +33,11 @@ const projects = [
     // },
 ]
 
-// const list = document.querySelector('.list');
-// const projectLength = projects.length;
+// const contendDiv = document.querySelector('.content');
+// projects.forEach((v,i)=>{
+//     const img = document.createElement('img');
+//     img.src=`/${v.name}/design/desktop-preview.jpg`;
+//     img.alt=`${v.name}`;
+//     contendDiv.appendChild(img);
 
-// projects.forEach(v=>{
-//     const listItem = document.createElement('div');
-//     listItem.className = 'list-item'
-//     listItem.innerHTML=`<a href=/${v.name}/index.html>
-//         <div class='instar'>
-//         </div>
-//         <img class="preview" src='/${v.name}/design/desktop-preview.jpg' alt="${v.name}" width='200' height='200'>
-//         <p>${v.name}</p>
-//     </a>
-//     `
-//     list.appendChild(listItem);
-// });
-
-
-const slider = document.querySelector('.slider');
-const imgholder = document.querySelector('.imgholder');
-const bullets = document.querySelector('.bullets');
-projects.forEach((v,i)=>{
-    const inputTag = document.createElement('input');
-    inputTag.type='radio';
-    inputTag.name='slide';
-    inputTag.id='slide'+(i+1);
-    slider.appendChild(inputTag);
-
-    const liTag = document.createElement('li');
-    liTag.innerHTML=`
-    <img src = '/${v.name}/design/desktop-preview.jpg' alt='${v.name}' width='200' height='200'>
-    `
-    imgholder.appendChild(liTag);
-
-    const labelTag = document.createElement('label');
-    labelTag.for='slide'+(i+1);
-    labelTag.innerText='&nbsp;'
-    bullets.appendChild(labelTag);
-})
+// })
