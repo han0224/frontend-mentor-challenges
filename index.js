0b03733c2c7294b1c61ee1b1d6315bf7b0b0cc60
han0224 <dbn0224@gmail.com>
2022-04-21 23:08:11 +0900

    0421

--- index.js
+++ index.js
@@ -25,18 +25,41 @@ const projects = [
     // },
 ]
 
-const list = document.querySelector('.list');
+// const list = document.querySelector('.list');
+// const projectLength = projects.length;
 
-projects.forEach(v=>{
-    const listItem = document.createElement('div');
-    listItem.className = 'list-item'
-    listItem.innerHTML=`<a href=/${v.name}/index.html>
-        <div class='instar'>
-        </div>
-        <img class="preview" src='/${v.name}/design/desktop-preview.jpg' alt="${v.name}" width='200' height='200'>
-        <p>${v.name}</p>
-    </a>
+// projects.forEach(v=>{
+//     const listItem = document.createElement('div');
+//     listItem.className = 'list-item'
+//     listItem.innerHTML=`<a href=/${v.name}/index.html>
+//         <div class='instar'>
+//         </div>
+//         <img class="preview" src='/${v.name}/design/desktop-preview.jpg' alt="${v.name}" width='200' height='200'>
+//         <p>${v.name}</p>
+//     </a>
+//     `
+//     list.appendChild(listItem);
+// });
+
+
+const slider = document.querySelector('.slider');
+const imgholder = document.querySelector('.imgholder');
+const bullets = document.querySelector('.bullets');
+projects.forEach((v,i)=>{
+    const inputTag = document.createElement('input');
+    inputTag.type='radio';
+    inputTag.name='slide';
+    inputTag.id='slide'+(i+1);
+    slider.appendChild(inputTag);
+
+    const liTag = document.createElement('li');
+    liTag.innerHTML=`
+    <img src = '/${v.name}/design/desktop-preview.jpg' alt='${v.name}' width='200' height='200'>
     `
-    list.appendChild(listItem);
+    imgholder.appendChild(liTag);
 
+    const labelTag = document.createElement('label');
+    labelTag.for='slide'+(i+1);
+    labelTag.innerText='&nbsp;'
+    bullets.appendChild(labelTag);
 })
